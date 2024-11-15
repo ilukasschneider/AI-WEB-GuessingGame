9b501aee8c159ac2f5d88e3de13912b6404a620b
Ally511 <131681237+Ally511@users.noreply.github.com>
2024-11-15 07:14:18 +0100

    timer in separate py

--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import time
+
 import altair as alt
 import numpy as np
 
@@ -115,22 +115,3 @@ st.write(f"**Maximum Guesses in a Game:** {df['Total Guesses'].max()}")
 st.write(f"**Minimum Guesses in a Game:** {df['Total Guesses'].min()}")
 
 
-#timer, can be used for the guessing game
-# Setze die Timer-Dauer in Sekunden
-timer_duration = 30
-
-# Button zum Starten des Timers
-if st.button("Start Timer"):
-    placeholder = st.empty()  # Platzhalter für die dynamische Timer-Anzeige
-    for remaining in range(timer_duration, 0, -1):
-        # Zeige den verbleibenden Timerwert
-        placeholder.write(f"Zeit verbleibend: {remaining} Sekunden")
-
-        # Warte eine Sekunde, bevor der Timer runterzählt
-        time.sleep(1)
-
-
-
-    # Zeige eine Nachricht, wenn der Timer endet
-    placeholder.write("Zeit abgelaufen!")
-    st.markdown("<h1 style='text-align: center; color: red; '>GAME OVER</h1>", unsafe_allow_html=True)
